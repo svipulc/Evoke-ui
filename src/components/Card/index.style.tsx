a376610d00337d64b4ae09638d5ecd9310eee394
svipulc <vipul.c@simformsolutions.com>
2024-09-25 14:00:35 +0530

    style: sm-91 remove border from card component

--- src/components/Card/index.style.tsx
+++ src/components/Card/index.style.tsx
@@ -4,21 +4,21 @@ export const cardStyle = cva([
   // css
   "flex flex-col justify-center",
   "w-full",
-  "rounded-lg border",
-  "bg-white dark:bg-primary dark:text-white dark:border-none",
+  "rounded-lg",
+  "bg-white dark:bg-primary dark:text-white ",
 ]);
 
 export const cardHeaderStyle = cva([
   // css
   "w-full",
-  "p-6",
+  "p-6 pb-0",
 ]);
 
 export const cardContentStyle = cva([
   // css
   "flex-grow",
   "w-full",
-  "p-6 pt-0",
+  "p-6",
 ]);
 
 export const cardFooterStyle = cva([
