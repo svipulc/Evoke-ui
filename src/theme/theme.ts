7755ef4eac083f6396ef7bd8acc7207b2d92dcd4
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-26 16:01:19 +0530

    refactor: change breakpoints value in default theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -96,9 +96,9 @@ export const shadows: ShadowsObject = {
 export const breakpoints: BreakpointsObject = {
   xs: "0",
   sm: "600px",
-  md: "900px",
-  lg: "1200px",
-  xl: "1536px",
+  md: "960px",
+  lg: "1280px",
+  xl: "1920px",
 };
 
 export const typography: TypographyObject = {
