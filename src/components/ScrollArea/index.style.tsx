f9f7ae926e0371a37dec64b5d6f7119bccf136d3
Kashyap2701 <kspatelsimform100@gmail.com>
2024-12-02 15:29:47 +0530

    feat: migrate scroll-area into emotion styling & make scrollbar visible in mobile screen

--- src/components/ScrollArea/index.style.tsx
+++ src/components/ScrollArea/index.style.tsx
@@ -1,17 +1,67 @@
-import { cva } from "class-variance-authority";
+import { CustomTheme } from "@/evoke-theme-config";
+import { css } from "@emotion/react";
 
-export const scrollAreaStyles = cva(
-  ["w-full", "h-full", "overflow-hidden", "scrollbar-container", "relative"],
-  {
-    variants: {
-      orientation: {
-        vertical: "hover:overflow-y-scroll",
-        horizontal: "hover:overflow-x-scroll",
-        both: "hover:overflow-auto",
+// Base styles for scroll area
+const scrollAreaBase = css({
+  width: "100%",
+  height: "100%",
+  overflow: "hidden",
+  position: "relative",
+  border: "solid 3px transparent",
+  scrollbarGutter: "stable both-edges",
+});
+
+// Scrollbar styles
+const scrollbarStyles = (theme: CustomTheme) =>
+  css({
+    "&::-webkit-scrollbar": {
+      width: "8px",
+      height: "8px",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "transparent",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      background: theme.colors.neutral.border,
+      borderRadius: theme.borderRadius.medium,
+    },
+  });
+
+// Orientation variant styles
+const orientationVariants = (theme: CustomTheme) => ({
+  vertical: css({
+    overflowY: "auto",
+    [`@media(min-width: ${theme.breakpoints.sm})`]: {
+      overflowY: "hidden",
+      "&:hover": {
+        overflowY: "scroll",
       },
     },
-    defaultVariants: {
-      orientation: "vertical",
+  }),
+  horizontal: css({
+    overflowX: "auto",
+    [`@media(min-width: ${theme.breakpoints.sm})`]: {
+      overflowX: "hidden",
+      "&:hover": {
+        overflowX: "scroll",
+      },
     },
-  }
-);
+  }),
+  both: css({
+    overflow: "auto",
+    [`@media(min-width: ${theme.breakpoints.sm})`]: {
+      overflow: "hidden",
+      "&:hover": {
+        overflow: "scroll",
+      },
+    },
+  }),
+});
+
+// Function to get the appropriate styles based on the orientation
+export const scrollAreaStyles = (
+  theme: CustomTheme,
+  orientation: "vertical" | "horizontal" | "both"
+) => {
+  return css([scrollAreaBase, scrollbarStyles(theme), orientationVariants(theme)[orientation]]);
+};
--- src/components/ScrollArea/index.tsx
+++ src/components/ScrollArea/index.tsx
@@ -1,17 +1,27 @@
+/** @jsxImportSource @emotion/react */
 import { ComponentProps, forwardRef } from "react";
-
-import { cn } from "@/utils";
 import { scrollAreaStyles } from "./index.style";
+import { CSSObject, SerializedStyles, useTheme } from "@emotion/react";
+import { CustomTheme } from "@/evoke-theme-config";
 
 type ScrollAreaProps = ComponentProps<"div"> & {
+  css?: SerializedStyles | CSSObject;
   children: React.ReactNode;
   orientation?: "horizontal" | "vertical" | "both";
 };
 
 export const ScrollArea = forwardRef<HTMLDivElement, ScrollAreaProps>(
-  ({ children, orientation = "vertical", className, ...props }, ref) => {
+  ({ css, children, orientation = "vertical", className, ...props }, ref) => {
+    const theme = useTheme() as CustomTheme;
+
     return (
-      <div {...props} ref={ref} className={cn(scrollAreaStyles({ orientation, className }))}>
+      <div
+        {...props}
+        ref={ref}
+        css={[scrollAreaStyles(theme, orientation), css]}
+        className={className}
+        role="region"
+      >
         {children}
       </div>
     );
