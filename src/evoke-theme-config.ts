53ae88f8f2f05981487d90c33c254a7ad56eb8d9
Kashyap2701 <kspatelsimform100@gmail.com>
2024-12-17 12:23:50 +0530

    refactor: add theme provider & change theme

--- src/evoke-theme-config.ts
+++ src/evoke-theme-config.ts
@@ -5,7 +5,9 @@ export type CustomTheme = typeof lightTheme;
 export const lightTheme = createTheme({
   colors: {
     neutral: {
-      text: "#ffffff",
+      text: "#000000",
+      background: "#ffffff",
+      border: "#d3d6db",
     },
     variants: {
       primary: {
@@ -31,14 +33,19 @@ export const lightTheme = createTheme({
 
 export const darkTheme = createTheme({
   colors: {
+    neutral: {
+      text: "#ffffff",
+      background: "#24293C",
+      border: "#4e5560",
+    },
     variants: {
       primary: {
-        main: "#24293C",
-        contrastText: "#ffffff",
-      },
-      secondary: {
         main: "#AACCFF",
         contrastText: "#24293C",
+      },
+      secondary: {
+        main: "#24293C",
+        contrastText: "#ffffff",
       },
       levender: {
         main: "#E6E6FA",
