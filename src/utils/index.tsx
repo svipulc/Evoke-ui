5b4e9b0818062f18bc7c255d973ee02bc87bbcf3
Kashyap2701 <kspatelsimform100@gmail.com>
2024-12-24 10:40:10 +0530

    feat(utils): add function to get responsive style

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,23 @@
+import { EvokeTheme, ResponsiveValue } from "@/theme/theme.type";
+import { css } from "@emotion/react";
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes));
+
+export function responsiveCss<T>(
+  theme: EvokeTheme,
+  value: ResponsiveValue<T>,
+  styleGetter: (value: T) => ReturnType<typeof css>
+) {
+  if (typeof value !== "object") {
+    return styleGetter(value);
+  }
+
+  return css(
+    Object.entries(value as object).map(([breakpoint, val]) => ({
+      [`@media (min-width: ${theme.breakpoints[breakpoint as keyof EvokeTheme["breakpoints"]]})`]:
+        styleGetter(val as T),
+    }))
+  );
+}
